cfa614c4e75f2701eed20474c6e1a0c9cd144b0e
twinkleypies <36009728+twinkleypies@users.noreply.github.com>
2018-01-31 11:05:22 -0800

    Completed assignment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,10 @@
-// Student name:
-// This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Gibble is the best pokemon");
+    }
+}// Student name: Jess Eaton
